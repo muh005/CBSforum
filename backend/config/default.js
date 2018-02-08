f55d419b8e524774cb71fcaec248c6ca6bc42f75
muh005 <muh005@ucsd.edu>
2018-02-08 00:18:09 -0800

    online server

--- backend/config/default.js
+++ backend/config/default.js
@@ -8,6 +8,6 @@ module.exports = {
         key: 'blog',
         maxAge: 2592000000
     },
-    mongodb:
+    mongodb:'mongodb://muh005:H%40rM0Ny!@muh005-shard-00-00-gk2jg.mongodb.net:27017,muh005-shard-00-01-gk2jg.mongodb.net:27017,muh005-shard-00-02-gk2jg.mongodb.net:27017/test?ssl=true&replicaSet=muh005-shard-0&authSource=admin'
 	//set the MongoDB connection string to access your new database.
 };
