c1a2a106eb9ea096c8da3bedb4d33ce182aee04d
Mu He <mu_he@outlook.com>
2018-02-19 14:17:28 -0800

    environment

--- frontend/src/app/shared/config/env.config.ts
+++ frontend/src/app/shared/config/env.config.ts
@@ -2,4 +2,4 @@
  * Created by Administrator on 2017/8/2.
  */
 
-export const SITE_HOST_URL: string = 'http://localhost:8000';
+export const SITE_HOST_URL: string = 'http://localhost:8000';//需要替换为服务器的IP地址
